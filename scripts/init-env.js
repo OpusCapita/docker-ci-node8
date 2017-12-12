b6c0fe90eb6c9217fcf323216c346b047d5e7d45
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-12-12 15:50:03 +0300

    #1 Fix passing env variables

--- scripts/init-env.js
+++ scripts/init-env.js
@@ -4,14 +4,13 @@ let run = require('./run');
 
 run(`
   mkdir /ci-vault &&
-  git clone --depth 1 git@github.com:OpusCapita/machineuser-vault.git /ci-vault
+  git clone git@github.com:OpusCapita/machineuser-vault.git /ci-vault
 `);
 
 run(`
   cd /ci-vault &&
   openssl aes-256-cbc -d -in .secrets -out .secrets-plain -k ${process.env.MACHINEUSER_VAULT_KEY}
 `);
-
 
 run(`
   echo "source /ci-vault/.secrets-plain" >> ${process.env.BASH_ENV};
